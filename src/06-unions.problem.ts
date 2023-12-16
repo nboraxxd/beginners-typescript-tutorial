8b33954fd466268546668cd23a519606a15bc4d0
bora <nhatbaoxxd@gmail.com>
2023-12-16 23:32:14 +0700

    Solver exercise 06-unions

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -8,13 +8,15 @@ interface User {
    * - 'user'
    * - 'super-admin'
    */
-  role: string;
+  role: 'admin' | 'user' | 'super-admin';
 }
 
 export const defaultUser: User = {
   id: 1,
   firstName: "Matt",
   lastName: "Pocock",
+  // ts-expect-error dùng để check lỗi ở dòng tiếp theo
+  // nếu dòng tiếp theo không có lỗi thì ts-expect-error sẽ báo
   // @ts-expect-error
   role: "I_SHOULD_NOT_BE_ALLOWED",
 };
