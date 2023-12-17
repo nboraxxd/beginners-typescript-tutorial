005fb3bd24957b6ad3139daac243f23ffbd12e6f
bora <nhatbaoxxd@gmail.com>
2023-12-17 09:06:03 +0700

    Solve exercie 09-promises

--- src/09-promises.problem.ts
+++ src/09-promises.problem.ts
@@ -9,10 +9,26 @@ interface LukeSkywalker {
   gender: string;
 }
 
-export const fetchLukeSkywalker = async (): LukeSkywalker => {
+export const fetchLukeSkywalker = async (): Promise<LukeSkywalker> => {
   const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
     return res.json();
   });
 
   return data;
+};
+
+export const fetchLukeSkywalker2 = async () => {
+  const data: LukeSkywalker = await fetch("https://swapi.dev/api/people/1").then((res) => {
+    return res.json();
+  });
+
+  return data;
+};
+
+export const fetchLukeSkywalker3 = async () => {
+  const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
+    return res.json();
+  });
+
+  return data as LukeSkywalker;
 };
